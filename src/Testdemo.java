a24bcf7237a3b756000ec2814544b01367a0a8e2
hahmad2384 <hahmad2384@gmail.com>
2019-06-27 22:03:03 -0400

    second commit

--- src/Testdemo.java
+++ src/Testdemo.java
@@ -4,6 +4,7 @@ public class Testdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My first code");
+System.out.println("My second code");
 	}
 
 }
