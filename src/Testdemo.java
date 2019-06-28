05c3b590c9ae52d7562b651bd68441d5795ca35d
Dannysorto1 <Dannysorto1@gmail.com>
2019-06-27 22:03:46 -0400

    New Code From Me

--- src/Testdemo.java
+++ src/Testdemo.java
@@ -2,8 +2,9 @@
 public class Testdemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("My first code");
+
+		System.out.println("My first code");
+		System.out.println("New Code From Danny");
 	}
 
 }
